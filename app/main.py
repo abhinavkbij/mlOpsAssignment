70180b74df04a5aba235f19754a013d4329315a3
Abhinav Bijarnia <abhinav789.bij@gmail.com>
2023-01-29 10:13:21 +0530

    updated upload file route

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from typing import Union
 
-from fastapi import FastAPI, File
+from fastapi import FastAPI, UploadFile
 from PIL import Image
 import tensorflow as tf
 import numpy as np
-
+import io
 app = FastAPI()
 
 
@@ -12,17 +12,21 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-
+# request affected by server, maybe nginx doing something, test first on local without tesnorflow, normal docker image of python or even without docker
 @app.post("/identifyDigit")
-def identifyDigit(img: bytes = File()):
+async def identifyDigit(imgFile: UploadFile):
+    fileContent = await imgFile.read()
+    img = Image.open(io.BytesIO(fileContent))
+    print (img)
+    # return imgFile.filename
     # convert image to array
-    imgArr = Image.frombytes(file)
+    imgArr = np.asarray(img, dtype='uint8')
 
-    imgArr = np.array(imgArr, dtype='uint8')
+    # imgArr = np.array(imgArr, dtype='uint8')
 
     print (imgArr)
     # load model
-    model = tf.keras.models.load_model('/code/app/saved_model1/')
+    model = tf.keras.models.load_model('./saved_model1/')
 
     # get predictions
-    model.predict(imgArr)
+    print (model.predict(imgArr))
